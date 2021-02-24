5651050b2cf7e8247ed472d81a4a79717a11ea72
mnpkoerner <mnpkoerner@gmail.com>
2021-02-23 21:04:38 -0600

    set up virtual environment

--- flaskintro.py
+++ flaskintro.py
@@ -1,22 +1,32 @@
 from flask import Flask, request
+import requests
+import psycopg2
+
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World'
 
 
-
 @app.route('/test')
 def hello_test():
     return 'Hello, test'
 
-@app.route('/get', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def one_requests():
     if request.method == 'GET':
-        return 'Made a GET request'
+        return '<h1>GET REQUEST</h1>'
     elif request.method == 'POST':
+        request.form('incoming')
+        print(data)
         return 'Made a POST request'
+
 
 @app.route('/put', methods=['PUT', 'DELETE'])
 def two_requests():
