886b92950ec0f0ed37e095c6ae31b470cd860ffa
mnpkoerner <mnpkoerner@gmail.com>
2021-02-23 18:37:11 -0600

    block of code responding to GET and POST

--- flaskintro.py
+++ flaskintro.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,3 +10,10 @@ def hello_world():
 @app.route('/test')
 def hello_test():
     return 'Hello, test'
+
+@app.route('/get', methods=['GET', 'POST'])
+def requests():
+    if request.method == 'GET':
+        return 'Made a GET request'
+    elif request.method == 'POST':
+        return 'Made a POST request'
