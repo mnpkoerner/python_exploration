94bed2772a44acfeccc27024303631c714d22934
mnpkoerner <mnpkoerner@gmail.com>
2021-02-23 18:43:51 -0600

    block of code responding to PUT and DELETE

--- flaskintro.py
+++ flaskintro.py
@@ -12,8 +12,15 @@ def hello_test():
     return 'Hello, test'
 
 @app.route('/get', methods=['GET', 'POST'])
-def requests():
+def one_requests():
     if request.method == 'GET':
         return 'Made a GET request'
     elif request.method == 'POST':
         return 'Made a POST request'
+
+@app.route('/put', methods=['PUT', 'DELETE'])
+def two_requests():
+    if request.method == 'PUT':
+        return 'Made a PUT request'
+    elif request.method == 'DELETE':
+        return 'Made a DELETE request'
